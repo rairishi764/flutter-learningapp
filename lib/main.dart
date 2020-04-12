9ba9f6ed16cf3b580e81806b1821f793576dc9db
Rishi <rai.rishi764@gmail.com>
2020-04-12 13:35:56 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';//import handled by pubsoec.yml
 
 //main function which is run first to kick of app code
 void main(){
-  runApp(MyApp());
+  runApp(MyApp()); /*fn defined in flutter package does all heavy lifiting of taking 
+                      a widget and put it on screen by calling the build fn of widget*/
   
 }
+
+//void main() => runApp(MyApp()); alternative syntax but only used in 1 widget run
+
+
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext ctx){
-    return MaterialApp(home: Text('Hello'),);
+  @override //good practic of specifying that u r overriding 
+  Widget build(BuildContext ctx){ //always responsible toreturn widget 
+    return MaterialApp(home: Scaffold(appBar: AppBar(title: Text('My first app',),centerTitle: true,))); //scaffold is widget that gives basic structure for app
   }
 }
