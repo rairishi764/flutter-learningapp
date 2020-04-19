8bfb807a3835589f0eb47a0a969310fe8c8af907
Rishi <rai.rishi764@gmail.com>
2020-04-19 11:09:57 +0100

    quiz completed

--- lib/question.dart
+++ lib/question.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  //every widget needs to be in class
+  final String questionText;
 
-  final String questionTest;
+  Question(this.questionText);
 
-  Question(this.questionTest);
-
-  Widget build(BuildContext ctx) {
-    // every widget class has to impleent build funtion
+  @override
+  Widget build(BuildContext context) {
     return Container(
-        // just a wrapper which isnt visible
-        width: double.infinity, //as much space available
-        margin: EdgeInsets.all(10),
-        padding: EdgeInsets.all(2),
-        child: Text(
-          questionTest,
-          style: TextStyle(fontSize: 28),
-          textAlign: TextAlign.center,
-        ));
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
